6c5e7701af8d5749072bb0f482bcd4a480ab9600
lingouitests <lingouitests@gmail.com>
2025-08-21 16:31:40 +0200

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "============================================================"
+echo "   Post Prep Kit Script START "
+echo "============================================================"
+echo
+echo "LRM_GROUP=${LRM_GROUP}"
+echo "LRM_PROJECT=${LRM_PROJECT}"
+echo "KIT_VERSION=${KIT_VERSION}"
+echo "LRM_LOCALE=${LRM_LOCALE}"
+echo "IMPORT_STATUS=${PREPKIT_STATUS}"
+
+
+
+echo THIS IS A POST PREPKIT SCRIPT
+echo "============================================================"
