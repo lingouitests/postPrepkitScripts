67afc20d65555aa84a9c099d24df032d70e5cffd
lingouitests <lingouitests@gmail.com>
2025-09-04 14:56:21 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,14 +3,5 @@
 echo "============================================================"
 echo "   Post Prep Kit Script START "
 echo "============================================================"
-echo
-echo "LRM_GROUP=${LRM_GROUP}"
-echo "LRM_PROJECT=${LRM_PROJECT}"
-echo "KIT_VERSION=${KIT_VERSION}"
-echo "LRM_LOCALE=${LRM_LOCALE}"
-echo "PREPKIT_STATUS=${PREPKIT_STATUS}"
-
-
-
-echo THIS IS A POST PREPKIT SCRIPT
+echo " THIS IS A POST PREPKIT SCRIPT "
 echo "============================================================"
